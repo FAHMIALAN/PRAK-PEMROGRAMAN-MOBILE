3c305a1e078d489bd9d2b3b191c120315281b5e4
FAHMI AL-ANSHORI <139199297+FAHMIALAN@users.noreply.github.com>
2024-10-17 10:14:10 +0700

    Datasource.kt

---
+++ PERTEMUAN 5/grid/Datasource.kt
@@ -0,0 +1,33 @@
+package com.example.grid.data
+
+import com.example.grid.R
+import com.example.grid.model.Topic
+
+object DataSource {
+    val topics = listOf(
+        Topic(R.string.architecture, 58, R.drawable.image1),
+        Topic(R.string.automotive, 30, R.drawable.image2),
+        Topic(R.string.biology, 90, R.drawable.image3),
+        Topic(R.string.crafts, 121, R.drawable.image4),
+        Topic(R.string.business, 78, R.drawable.image5),
+        Topic(R.string.culinary, 118, R.drawable.image6),
+        Topic(R.string.design, 423, R.drawable.image7),
+        Topic(R.string.ecology, 28, R.drawable.image8),
+        Topic(R.string.engineering, 67, R.drawable.image9),
+        Topic(R.string.fashion, 92, R.drawable.image10),
+        Topic(R.string.finance, 100, R.drawable.image11),
+        Topic(R.string.film, 165, R.drawable.image12),
+        Topic(R.string.gaming, 37, R.drawable.image13),
+        Topic(R.string.geology, 290, R.drawable.image14),
+        Topic(R.string.drawing, 326, R.drawable.image15),
+        Topic(R.string.history, 189, R.drawable.image16),
+        Topic(R.string.journalism, 96, R.drawable.image17),
+        Topic(R.string.law, 58, R.drawable.image18),
+        Topic(R.string.lifestyle, 305, R.drawable.image19),
+        Topic(R.string.music, 212, R.drawable.image20),
+        Topic(R.string.painting, 172, R.drawable.image21),
+        Topic(R.string.photography, 321, R.drawable.image22),
+        Topic(R.string.physics, 41, R.drawable.image23),
+        Topic(R.string.tech, 118, R.drawable.image24),
+    )
+}
